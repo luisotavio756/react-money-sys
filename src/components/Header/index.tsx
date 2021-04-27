c4eeeed10e56177f3dbc71811b447f138388ffb2
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-27 11:13:49 -0300

    Fix eslint errors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Logo from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
-const Header: React.FC = () => {
-  return (
-    <Container>
-      <Content>
-        <img src={Logo} alt="dt money"/>
-        <button type="button">Nova transação</button>
-      </Content>
-    </Container>
-  );
-}
+const Header: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={Logo} alt="dt money" />
+      <button type="button">Nova transação</button>
+    </Content>
+  </Container>
+);
 
 export default Header;
