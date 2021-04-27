2151f145edda46a19f56fee61f71274ed6549c68
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-27 10:58:11 -0300

    Fix spacing in global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,56 +1,56 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-    :root {
-        --background: #f0f2f5;
-        --red: #e52e4d;
-        --blue: #5429cc;
-        --green: #33cc95;
+  :root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --green: #33cc95;
 
-        --blue-light: #6933ff;
+    --blue-light: #6933ff;
 
-        --text-title: #363f5f;
-        --text-body: #969cb3;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
 
-        --shape: #ffffff;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
     }
 
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+    @media (max-width: 720px) {
+      font-size: 87.5%;
     }
+  }
 
-    html {
-        @media (max-width: 1080px) {
-            font-size: 93.75%;
-        }
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
 
-        @media (max-width: 720px) {
-            font-size: 87.5%;
-        }
-    }
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
 
-    body {
-        background: var(--background);
-        -webkit-font-smoothing: antialiased;
-    }
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
 
-    body, input, textarea, button {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
-    }
+  button {
+    cursor: pointer;
+  }
 
-    h1, h2, h3, h4, h5, h6, strong {
-        font-weight: 600;
-    }
-
-    button {
-        cursor: pointer;
-    }
-
-    [disabled] {
-        opacity: 0.6;
-        cursor: not-allowed;
-    }
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
 `;
