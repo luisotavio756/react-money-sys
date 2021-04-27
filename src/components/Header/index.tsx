aede8fd9e4161bff61d26e377e8c24cb56de9070
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-27 10:40:16 -0300

    Create Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import Logo from '../../assets/logo.svg';
+import { Container, Content } from './styles';
 
 const Header: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="dt money"/>
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
 }
 
 export default Header;
