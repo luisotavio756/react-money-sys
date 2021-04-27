7adcf7b581fce62ffb7dd51e4e3d0928b2c9fbcf
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-27 10:17:35 -0300

    Config css vars colors

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;    
+        --red: #e52e4d;
+        --blue: #5429cc;
+
+        --blue-light: #6933ff;
+
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --shape: #ffffff; 
     }
 
     * {
@@ -10,9 +19,28 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
+    
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%;
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%;
+        }
+    }
 
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
     }
 `;
