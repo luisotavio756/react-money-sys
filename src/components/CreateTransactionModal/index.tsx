87e29ffff474178a6b01cee5b4cff16db3bef15c
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-28 16:00:52 -0300

    Made modal updates

--- src/components/CreateTransactionModal/index.tsx
+++ src/components/CreateTransactionModal/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '../Modal';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
@@ -12,41 +12,55 @@ interface ICreateTransactionModalProps {
 const CreateTransactionModal: React.FC<ICreateTransactionModalProps> = ({
   handleCloseNewTransactionModal,
   isNewTransactionModalOpen,
-}) => (
-  <Modal
-    isOpen={isNewTransactionModalOpen}
-    title="Cadastrar transação"
-    setIsOpen={handleCloseNewTransactionModal}
-  >
-    <Container>
-      <input
-        type="text"
-        placeholder="Título"
-      />
+}) => {
+  const [type, setType] = useState('deposit');
 
-      <input
-        type="number"
-        placeholder="Valor"
-      />
-      <TransactionTypeContainer>
-        <button type="button">
-          <img src={incomeImg} alt="Entrada" />
-          <span>Entrada</span>
-        </button>
-        <button type="button">
-          <img src={outcomeImg} alt="Saída" />
-          <span>Saída</span>
-        </button>
-      </TransactionTypeContainer>
+  return (
+    <Modal
+      isOpen={isNewTransactionModalOpen}
+      title="Cadastrar transação"
+      setIsOpen={handleCloseNewTransactionModal}
+    >
+      <Container>
+        <input
+          type="text"
+          placeholder="Título"
+        />
 
-      <input
-        type="text"
-        placeholder="Categoria"
-      />
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+            type="button"
+            onClick={() => setType('outdraw')}
+            isActive={type === 'outdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
 
-      <button type="submit">Cadastrar</button>
-    </Container>
-  </Modal>
-);
+        <input
+          type="text"
+          placeholder="Categoria"
+        />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+};
 
 export default CreateTransactionModal;
--- src/components/CreateTransactionModal/styles.ts
+++ src/components/CreateTransactionModal/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
+
+interface IRadioBoxProps {
+  isActive: boolean;
+  activeColor: 'red' | 'green';
+}
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d',
+};
 
 export const Container = styled.form`
   margin-top: 2rem;
@@ -15,6 +25,7 @@ export const Container = styled.form`
 
     font-weight: 400;
     font-size: 1rem;
+    outline: none;
 
     &::placeholder {
       color: var(--text-body);
@@ -50,32 +61,34 @@ export const TransactionTypeContainer = styled.div`
   margin: 1rem 0;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-column-gap: 1rem;
+  grid-column-gap: 0.5rem;
+`;
 
-  button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button<IRadioBoxProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: transparent;
-    transition: border-color 0.2s;
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) => (props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent')};
+  outline: none;
+  transition: border-color 0.2s;
 
-    img {
-      margin-right: 0.25rem;
-      height: 20px;
-      width: 20px;
-    }
-
-    span {
-      font-size: 1rem;
-      color: var(--text-title);
-    }
-
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')}
-    }
+  img {
+    margin-right: 0.25rem;
+    height: 20px;
+    width: 20px;
   }
+
+  span {
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')}
+  }
+
 `;
