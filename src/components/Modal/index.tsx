6026737c129fc3150e306835db4c81bbc3fd48d5
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-28 10:10:12 -0300

    Create modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import { FiX } from 'react-icons/fi';
+import ReactModal from 'react-modal';
+import { CloseContainer } from './styles';
+
+interface IModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  title: string;
+}
+
+const Modal: React.FC<IModalProps> = ({
+  children,
+  isOpen,
+  title,
+  setIsOpen,
+}) => {
+  const [modalStatus, setModalStatus] = useState(isOpen);
+
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
+
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={modalStatus}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#312e38',
+          color: '#000000',
+          borderRadius: '8px',
+          maxWidth: '736px',
+          width: '100%',
+          border: 'none',
+          height: '600px',
+          overflowY: 'auto',
+        },
+        overlay: {
+          backgroundColor: '#23212973',
+        },
+      }}
+    >
+      <CloseContainer>
+        <h1>{title}</h1>
+        <button type="button" onClick={() => setIsOpen()}>
+          <FiX />
+        </button>
+      </CloseContainer>
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
---
+++ src/components/Modal/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const CloseContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 36px;
+  }
+  button {
+    background: transparent;
+    color: #fff;
+    border: none;
+    svg {
+      font-size: 22px;
+    }
+  }
+`;
