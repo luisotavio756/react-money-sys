6b2d53994af17eb0db1a5a6b2a26cd2489a42b52
Luis Otavio <luis.otavio2425@gmail.com>
2021-04-28 10:00:16 -0300

    Configrue miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import App from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => [
+      {
+        id: 1,
+        title: 'Transaction one',
+        amount: 400,
+        type: 'deposit',
+        category: 'Food',
+        createdAt: new Date(),
+      },
+    ]);
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
